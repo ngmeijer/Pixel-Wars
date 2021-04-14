0ecb828c8b4a69fa6e0357efa63ace40154b169f
Nils Meijer <meijernils9@gmail.com>
2021-04-14 12:14:52 +0200

    Found performance issues after building to Android tablet

--- Pixel Wars/Assets/Scripts/ArmyUnit.cs
+++ Pixel Wars/Assets/Scripts/ArmyUnit.cs
@@ -14,9 +14,9 @@ public class ArmyUnit : ScriptableObject
 {
     public ArmyColourEnum ColourEnum;
     public BATTLE_MODE BattleMode;
-    [Range(0.1f, 5f)] public float RespawnRate;
-    [Range(0.1f, 5f)] public float AttackSpeed;
-    [Range(10, 1000)] public int BaseHealth;
-    [Range(0, 10)] public int MaxAmountOfUnits;
+    [Range(0.1f, 5f)] public float RespawnRate = 0.8f;
+    [Range(0.1f, 5f)] public float AttackSpeed = 1f;
+    [Range(10, 1000)] public int BaseHealth = 300;
+    [Range(0, 8)] public int MaxAmountOfUnits = 5;
     public GameObject LegionPrefab;
 }
